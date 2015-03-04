4749d70a2b926d4aedb5cc1b2f0ed4118b32be40
Mateusz Kwasniewski <mateusz.kwasniewski@schibsted.pl>
2015-03-04 13:47:34 +0100

    extract sources from esi tags

--- esi.js
+++ esi.js
@@ -1,7 +1,15 @@
 var Q = require('q');
+var cheerio = require("cheerio");
 
 exports.process = function(html) {
     var deferred = Q.defer();
+
+    var $ = cheerio.load(html);
+    var sources = $('esi\\:include').map(function() {
+        return $(this).attr('src');
+    }).get();
+    console.log(sources);
+
     deferred.resolve('<div>test</div>');
     return deferred.promise;
 }
