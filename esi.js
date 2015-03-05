25ea822e44fcfad7f93862c8f28ca1bf762c28bc
Krystian Jarmicki <jarmicki@gmail.com>
2015-03-05 11:21:05 +0100

    cleanup

--- esi.js
+++ esi.js
@@ -7,28 +7,6 @@ var cheerio = require('cheerio'),
     request = require('request'),
     url = require('url');
 
-var get = function(options) {
-    return new Promise(function(resolve, reject) {
-        request.get(options, function(error, response, body) {
-            if(error || response.statusCode >= 400) {
-                resolve('');
-            }
-            else {
-                resolve(body);
-            }
-        });
-    });
-};
-
-var toFullyQualifiedURL = function(base, urlOrPath) {
-    if(urlOrPath.indexOf('http') === 0) {
-        return urlOrPath;
-    } else {
-        return url.resolve(base, urlOrPath);
-    }
-};
-
-
 function ESI(config) {
     config = config || {};
 
@@ -43,6 +21,28 @@ ESI.prototype.makeRequestOptions = function(url) {
     };
 };
 
+ESI.prototype.toFullyQualifiedURL = function(base, urlOrPath) {
+    if(urlOrPath.indexOf('http') === 0) {
+        return urlOrPath;
+    } else {
+        return url.resolve(base, urlOrPath);
+    }
+};
+
+ESI.prototype.get = function(options) {
+    return new Promise(function(resolve, reject) {
+        request.get(options, function(error, response, body) {
+            if(error || response.statusCode >= 400) {
+                resolve('');
+            }
+            else {
+                resolve(body);
+            }
+        });
+    });
+};
+
+
 ESI.prototype.process = function(html) {
     var self = this;
 
@@ -51,9 +51,9 @@ ESI.prototype.process = function(html) {
             sources = $('esi\\:include').map(function() {
                 return $(this).attr('src');
             }).get(),
-            urls = sources.map(toFullyQualifiedURL.bind(null, self.basePath));
+            urls = sources.map(self.toFullyQualifiedURL.bind(null, self.basePath));
 
-        Promise.all(urls.map(self.makeRequestOptions.bind(self)).map(get)).then(function(results) {
+        Promise.all(urls.map(self.makeRequestOptions.bind(self)).map(self.get)).then(function(results) {
             results.forEach(function(result) {
                 $('esi\\:include').first().replaceWith(result);
             });
