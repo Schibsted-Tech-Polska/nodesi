20c4cd14da244e258f2fb751bbf0da7c415219e0
Krystian Jarmicki <jarmicki@gmail.com>
2015-03-11 01:20:20 +0100

    spawning siege as child process

--- perf.js
+++ perf.js
@@ -2,16 +2,27 @@
 
 'use strict';
 
-var PORT = 3000,
-    ADDRESS = 'http://localhost';
+var PORT = 3003,
+    ADDRESS = 'http://localhost',
 
-var ESI = require('./index'),
+    ESI = require('./index'),
+    cp = require('child_process'),
     express = require('express'),
+
     app = express(),
-    
     esi = new ESI({
         baseUrl: ADDRESS + ':' + PORT
-    });
+    }),
+    siege,
+    server,
+
+    spawn = function(proc, args, options) {
+        if(process.platform === 'win32') {
+            return cp.spawn(process.env.comspec, ['/c'].concat(proc).concat(args), options);
+        } else {
+            return cp.spawn(proc, args, options);
+        }
+    };
 
 app.get('/test', function(req, res) {
     esi.process('<section><esi:include src="/test2"></esi:include></section>').then(res.send.bind(res));
@@ -21,5 +32,13 @@ app.get('/test2', function(req, res) {
     res.send('<div>hello</div>');
 });
 
-app.listen(PORT);
-console.log('Performance test server listening at ' + ADDRESS + ':' + PORT + '/test');
+app.listen(PORT, function() {
+    console.log('Performance test server listening at ' + ADDRESS + ':' + PORT + '/test');
+
+    siege = spawn('siege', ['-b', '-t5s', ADDRESS + ':' + PORT + '/test'], {
+        stdio: 'inherit'
+    });
+    siege.on('exit', function() {
+        process.nextTick(process.exit);
+    });
+});
